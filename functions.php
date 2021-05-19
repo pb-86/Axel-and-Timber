9dca483cd8eeb466abbc794eb658d434a809374d
Przemek Bąchorek <p.bachorek@gmail.com>
2021-05-19 12:39:20 +0200

    1.0.8
    Added: support for target attribute in menu links

--- functions.php
+++ functions.php
@@ -4,10 +4,10 @@
  *
  * @package WordPress
  * @subpackage Axel-Twig
- * @since 1.0.7
+ * @since 1.0.8
  */
 
-define( 'AXEL_THEME_VERSION', '1.0.7' );
+define( 'AXEL_THEME_VERSION', '1.0.8' );
 
 define( 'AXEL_THEME_URI', get_template_directory_uri() );
 define( 'AXEL_THEME_IMAGES', AXEL_THEME_URI . '/assets/images/compressed/' );

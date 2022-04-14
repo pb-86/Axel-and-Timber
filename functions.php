985b65c2fa69c90a2e08e37e58c1d4f958d07a86
Przemek Bąchorek <p.bachorek@gmail.com>
2022-04-14 12:49:26 +0200

    Changed functions.php file for better code readability

--- functions.php
+++ functions.php
@@ -4,23 +4,35 @@
  *
  * @package WordPress
  * @subpackage Axel and Timber
- * @since 1.13
+ * @since 1.14
  */
 
-define( 'AXEL_THEME_VERSION', '1.12' );
+define( 'AXEL_THEME_VERSION', '1.14' );
 
+// Defining useful constants for further use.
 define( 'AXEL_THEME_URI', get_template_directory_uri() );
 define( 'AXEL_THEME_IMAGES', AXEL_THEME_URI . '/assets/images/compressed/' );
 define( 'AXEL_THEME_STYLES', AXEL_THEME_URI . '/assets/styles/css/' );
 define( 'AXEL_THEME_SCRIPTS', AXEL_THEME_URI . '/assets/scripts/minified/' );
 define( 'AXEL_THEME_VENDOR', AXEL_THEME_URI . '/assets/vendor/' );
 
-require_once 'settings/setup.php';        // Basic theme setup.
-require_once 'settings/styles.php';       // Registering CSS styles.
-require_once 'settings/scripts.php';      // Registering JS scripts.
-require_once 'settings/menus.php';        // Registering menus.
-require_once 'settings/sidebars.php';     // Registering sidebar.
-require_once 'settings/optimisation.php'; // Code that helps speed up site.
+// Basic theme setup.
+require_once 'settings/setup.php';
+
+// Registering CSS styles.
+require_once 'settings/styles.php';
+
+// Registering JS scripts.
+require_once 'settings/scripts.php';
+
+// Registering menus.
+require_once 'settings/menus.php';
+
+// Registering sidebars.
+require_once 'settings/sidebars.php';
+
+// Code that helps speed up site.
+require_once 'settings/optimisation.php';
 
 /**
  * Adds additional data to the main Timber context.
